345c7ac28a85b2a062fbacb70777c80445c22b8c
root <root@tonnect.aeza.network>
2023-01-21 17:49:18 +0300

    added app_id

--- bot/app/keyboards.py
+++ bot/app/keyboards.py
@@ -7,7 +7,8 @@ def connect_markup(bot_username: str) -> InlineKeyboardMarkup:
     :param bot_username: bot username without `@`
     :return: :class:`InlineKeyboardMarkup`
     """
-    base_url = "http://tonapi.io/login?return_url={}"
+    app_id = "41ce47914bffe24cc5979b4c1b36ec5a6960ccfd480d2a567f7aa38284a1560031373033"
+    base_url = "http://tonapi.io/login?app={}&return_url={}"
     return_url = "https://tonnect.ru/api/login/bot={}"
 
     return InlineKeyboardMarkup(
@@ -15,7 +16,7 @@ def connect_markup(bot_username: str) -> InlineKeyboardMarkup:
             [
                 InlineKeyboardButton(
                     text="Connect Tonkeeper",
-                    url=base_url.format(return_url.format(bot_username))
+                    url=base_url.format(app_id, return_url.format(bot_username))
                 )
             ]
         ]
